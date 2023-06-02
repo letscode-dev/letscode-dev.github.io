fb36f4ba26a163c3dbe04d43a871a29403cca507
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-06-02 12:09:11 +0400

    z

--- interview/vue/starter.js
+++ interview/vue/starter.js
@@ -10,3 +10,20 @@ new Vue({
         this.menu = ARR_MENU_LINKS;
     },
 });
+
+const userId = '272620381';
+const token = '6205367184:AAHZZF3vpzohA4AlZzQk3vLzEfNZEVq8gfk';
+const message = 'O brave new world';
+
+const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${userId}&text=${message}`;
+
+const element = document.querySelector('#send');
+element.addEventListener('click', action);
+
+function action() {
+    console.log('fetch');
+
+    fetch(url)
+        .then(res => res.json())
+        .then(body => console.log(body))
+}
