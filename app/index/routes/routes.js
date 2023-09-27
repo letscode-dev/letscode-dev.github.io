341a3139cac0889dbc1e63660f74e10c35ddc0ed
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-09-27 16:46:36 +0400

    Bash Script Commit

--- app/index/routes/routes.js
+++ app/index/routes/routes.js
@@ -1,70 +1,70 @@
 export const MENU_ROUTES = {
     letsCode: {
-        path: '/',
-        title: 'Let\'s Code School'
+        path: "/",
+        title: "Let's Code School",
     },
     letsInterview: {
-        path: '/interview',
-        title: 'Let\'s Interview'
-    }
-}
+        path: "/interview",
+        title: "Let's Interview",
+    },
+};
 
 export const MENU_HASH = {
     cources: {
-        hash: 'cources',
-        title: 'Курсы',
+        hash: "cources",
+        title: "Курсы",
     },
     publications: {
-        hash: 'publications',
-        title: 'Публикации',
-    }
-}
+        hash: "publications",
+        title: "Публикации",
+    },
+};
 
 export const MENU_SELECTED = {
     community: {
-        title: 'Коммьюнити',
+        title: "Комьюнити",
         items: [
             {
-                href: 'https://t.me/letscode_channel',
-                title: 'Telegram Channel'
+                href: "https://t.me/letscode_channel",
+                title: "Telegram Channel",
             },
             {
-                href: 'https://t.me/letscode_chat',
-                title: 'Telegram Chat'
+                href: "https://t.me/letscode_chat",
+                title: "Telegram Chat",
             },
             {
-                href: 'https://t.me/letscodedev_bot',
-                title: 'Telegram Bot'
+                href: "https://t.me/letscodedev_bot",
+                title: "Telegram Bot",
             },
             {
-                href: 'https://anton-sergeenkov.github.io',
-                title: 'Об авторе'
+                href: "https://anton-sergeenkov.github.io",
+                title: "Об авторе",
             },
-        ]
+        ],
     },
     learn: {
-        title: 'Обучение',
+        title: "Обучение",
         items: [
             {
-                href: 'https://www.youtube.com/c/letscode-dev',
-                title: 'YouTube'
+                href: "https://www.youtube.com/c/letscode-dev",
+                title: "YouTube",
             },
             {
-                href: 'https://letscode-dev.github.io/workbook',
-                title: 'Методичка'
+                href: "https://letscode-dev.github.io/workbook",
+                title: "Методичка",
             },
             {
-                href: 'https://github.com/letscode-dev',
-                title: 'GitHub'
+                href: "https://github.com/letscode-dev",
+                title: "GitHub",
             },
             {
-                href: 'https://codepen.io/letscode-dev',
-                title: 'CodePen'
+                href: "https://codepen.io/letscode-dev",
+                title: "CodePen",
             },
             {
-                href: 'https://codesandbox.io/u/letscode-dev',
-                title: 'CodeSandbox'
+                href: "https://codesandbox.io/u/letscode-dev",
+                title: "CodeSandbox",
             },
-        ]
-    }
-}
+        ],
+    },
+};
