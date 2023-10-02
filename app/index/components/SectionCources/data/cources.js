d1124ca19432dad324aa98182b09b8b66145a20c
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2023-10-02 11:51:24 +0400

    Bash Script Commit

--- app/index/components/SectionCources/data/cources.js
+++ app/index/components/SectionCources/data/cources.js
@@ -1,126 +1,158 @@
-import coverReactStarWars from './img/react-star-wars.png'
-import coverJsModuleComics from './img/js-modules-comics.png'
-import coverJsGuitarShop from './img/js-guitar-shop.jpg'
-import coverGit from './img/git.png'
-import coverFrontend from './img/frontend.jpg'
-import coverMarkup from './img/markup.png'
+import coverReactStarWars from "./img/react-star-wars.png";
+import coverJsModuleComics from "./img/js-modules-comics.png";
+import coverJsGuitarShop from "./img/js-guitar-shop.jpg";
+import coverGit from "./img/git.png";
+import coverFrontend from "./img/frontend.jpg";
+import coverMarkup from "./img/markup.png";
+import coverReactArrowGame from "./img/react-arrow-game.jpg";
 
 export const COURCES = [
     {
-        stack: 'Git и GitHub',
-        title: 'Теоретический курс',
-        youtube: 'https://www.youtube.com/playlist?list=PL7cTIfGFrdKnPyWTcz_GEQo5drpr_1UH2',
-        level: 'Начальный',
+        stack: "React и Redux Toolkit",
+        title: "Приложение ArrowGame",
+        youtube:
+            "https://www.youtube.com/playlist?list=PL7cTIfGFrdKmWvaZph1HnoWIcVabcowzE",
+        level: "Средний / Продвинутый",
+        time: "5 часов",
+        img: coverReactArrowGame,
+        type: "Практика",
+        links: [
+            {
+                name: "Курс на YouTube",
+                link: "https://www.youtube.com/playlist?list=PL7cTIfGFrdKmWvaZph1HnoWIcVabcowzE",
+            },
+            {
+                name: "Деплой проекта",
+                link: "https://letscode-dev.github.io/react-arrow-game/",
+            },
+            {
+                name: "Репозиторий проекта",
+                link: "https://github.com/letscode-dev/react-arrow-game",
+            },
+        ],
+    },
+
+    {
+        stack: "Git и GitHub",
+        title: "Теоретический курс",
+        youtube:
+            "https://www.youtube.com/playlist?list=PL7cTIfGFrdKnPyWTcz_GEQo5drpr_1UH2",
+        level: "Начальный",
         time: null,
         img: coverGit,
-        type: 'Практика',
+        type: "Практика",
         links: [
             {
-                name: 'Курс на YouTube',
-                link: 'https://www.youtube.com/playlist?list=PL7cTIfGFrdKnPyWTcz_GEQo5drpr_1UH2'
+                name: "Курс на YouTube",
+                link: "https://www.youtube.com/playlist?list=PL7cTIfGFrdKnPyWTcz_GEQo5drpr_1UH2",
             },
         ],
     },
     {
-        stack: 'React и Redux',
-        title: 'Приложение по Star Wars',
-        youtube: 'https://www.youtube.com/playlist?list=PL7cTIfGFrdKkQAWKDu2NdFt5Cx38B-A3i',
-        level: 'Продвинутый',
-        time: '15 часов 35 минут',
+        stack: "React и Redux",
+        title: "Приложение по Star Wars",
+        youtube:
+            "https://www.youtube.com/playlist?list=PL7cTIfGFrdKkQAWKDu2NdFt5Cx38B-A3i",
+        level: "Продвинутый",
+        time: "15 часов 35 минут",
         img: coverReactStarWars,
-        type: 'Практика',
+        type: "Практика",
         links: [
             {
-                name: 'Курс на YouTube',
-                link: 'https://www.youtube.com/playlist?list=PL7cTIfGFrdKkQAWKDu2NdFt5Cx38B-A3i'
+                name: "Курс на YouTube",
+                link: "https://www.youtube.com/playlist?list=PL7cTIfGFrdKkQAWKDu2NdFt5Cx38B-A3i",
             },
             {
-                name: 'Проект на GitHub Pages',
-                link: 'https://letscode-dev.github.io/react-star-wars/'
-            },
-            { 
-                name: 'Репозиторий на GitHub',
-                link: 'https://github.com/letscode-dev/react-star-wars'
+                name: "Деплой проекта",
+                link: "https://letscode-dev.github.io/react-star-wars/",
             },
             {
-                name: 'StoryBook на GitHub Pages',
-                link: 'https://letscode-dev.github.io/react-star-wars/storybook'
+                name: "Репозиторий проекта",
+                link: "https://github.com/letscode-dev/react-star-wars",
+            },
+            {
+                name: "StoryBook",
+                link: "https://letscode-dev.github.io/react-star-wars/storybook",
             },
         ],
     },
     {
-        stack: 'JavaScript ES6-модули',
-        title: 'Приложение по комиксам Marvel',
-        youtube: 'https://www.youtube.com/playlist?list=PL7cTIfGFrdKmKK4xAJsE31n3SACY0bi9t',
-        level: 'Средний',
-        time: '3 часа 52 минуты',
+        stack: "JavaScript ES6-модули",
+        title: "Приложение по комиксам Marvel",
+        youtube:
+            "https://www.youtube.com/playlist?list=PL7cTIfGFrdKmKK4xAJsE31n3SACY0bi9t",
+        level: "Средний",
+        time: "3 часа 52 минуты",
         img: coverJsModuleComics,
-        type: 'Практика',
+        type: "Практика",
         links: [
             {
-                name: 'Курс на YouTube',
-                link: 'https://www.youtube.com/playlist?list=PL7cTIfGFrdKmKK4xAJsE31n3SACY0bi9t'
+                name: "Курс на YouTube",
+                link: "https://www.youtube.com/playlist?list=PL7cTIfGFrdKmKK4xAJsE31n3SACY0bi9t",
             },
             {
-                name: 'Проект на GitHub Pages',
-                link: 'https://letscode-dev.github.io/js-modules-comics'
+                name: "Деплой проекта",
+                link: "https://letscode-dev.github.io/js-modules-comics",
             },
             {
-                name: 'Репозиторий на GitHub',
-                link: 'https://github.com/letscode-dev/js-modules-comics'
+                name: "Репозиторий проекта",
+                link: "https://github.com/letscode-dev/js-modules-comics",
             },
         ],
     },
     {
-        stack: 'JavaScript ES6-классы',
-        title: 'Приложение для магазина гитар',
-        youtube: 'https://www.youtube.com/playlist?list=PL7cTIfGFrdKn9lnGXEUWEXYdYp-HKGPAw',
-        level: 'Начальный',
-        time: '2 часа 33 минуты',
+        stack: "JavaScript ES6-классы",
+        title: "Приложение для магазина гитар",
+        youtube:
+            "https://www.youtube.com/playlist?list=PL7cTIfGFrdKn9lnGXEUWEXYdYp-HKGPAw",
+        level: "Начальный",
+        time: "2 часа 33 минуты",
         img: coverJsGuitarShop,
-        type: 'Практика',
+        type: "Практика",
         links: [
             {
-                name: 'Курс на YouTube',
-                link: 'https://www.youtube.com/playlist?list=PL7cTIfGFrdKn9lnGXEUWEXYdYp-HKGPAw'
+                name: "Курс на YouTube",
+                link: "https://www.youtube.com/playlist?list=PL7cTIfGFrdKn9lnGXEUWEXYdYp-HKGPAw",
             },
             {
-                name: 'Проект на GitHub Pages',
-                link: 'https://letscode-dev.github.io/js-guitar-shop'
+                name: "Деплой проекта",
+                link: "https://letscode-dev.github.io/js-guitar-shop",
             },
             {
-                name: 'Репозиторий на GitHub',
-                link: 'https://github.com/letscode-dev/js-guitar-shop'
+                name: "Репозиторий проекта",
+                link: "https://github.com/letscode-dev/js-guitar-shop",
             },
         ],
     },
     {
-        stack: 'Frontend',
-        title: 'Отдельные видео по Frontend',
-        youtube: 'https://www.youtube.com/playlist?list=PL7cTIfGFrdKnuc0VJqAajaRffTjdLA-jQ',
-        level: 'Все',
+        stack: "Frontend",
+        title: "Отдельные видео по Frontend",
+        youtube:
+            "https://www.youtube.com/playlist?list=PL7cTIfGFrdKnuc0VJqAajaRffTjdLA-jQ",
+        level: "Все",
         img: coverFrontend,
-        type: 'Теория и практика',
+        type: "Теория и практика",
         links: [
             {
-                name: 'Курс на YouTube',
-                link: 'https://www.youtube.com/playlist?list=PL7cTIfGFrdKnuc0VJqAajaRffTjdLA-jQ'
+                name: "Курс на YouTube",
+                link: "https://www.youtube.com/playlist?list=PL7cTIfGFrdKnuc0VJqAajaRffTjdLA-jQ",
             },
         ],
     },
     {
-        stack: 'HTML5 и CSS3',
-        title: 'Теоретический курс',
-        youtube: 'https://www.youtube.com/playlist?list=PL7cTIfGFrdKkmf-c43pmhzH3otzUCl8f3',
-        level: 'Все',
+        stack: "HTML5 и CSS3",
+        title: "Теоретический курс",
+        youtube:
+            "https://www.youtube.com/playlist?list=PL7cTIfGFrdKkmf-c43pmhzH3otzUCl8f3",
+        level: "Все",
         time: null,
         img: coverMarkup,
-        type: 'Теория',
+        type: "Теория",
         links: [
             {
-                name: 'Курс на YouTube',
-                link: 'https://www.youtube.com/playlist?list=PL7cTIfGFrdKkmf-c43pmhzH3otzUCl8f3'
+                name: "Курс на YouTube",
+                link: "https://www.youtube.com/playlist?list=PL7cTIfGFrdKkmf-c43pmhzH3otzUCl8f3",
             },
         ],
     },
-]
+];
